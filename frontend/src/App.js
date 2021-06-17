31bcc89cf5b5df108a6325a2464beadea23126be
= <=>
2021-06-17 02:44:24 +0100

    added some features to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import Reactt from 'react';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App=() => {
   return (
     <>
-      <h1>Welcome to quick buy</h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+            <h1>Welcome to quick buy</h1>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 }
